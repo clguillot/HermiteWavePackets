169157c9827d164d4f822874c27e0584721f3562
Clement Guillot <clement.guillot@enpc.fr>
2024-11-24 18:17:08 +0100

    Removed custom math functions and defined the derivative manually

--- src/math.jl
+++ src/math.jl
@@ -1,34 +1,24 @@
-# Complex exponential compatible with autodiff
-@inline function myexp(x::Real)
-    return exp(x)
-end
-@inline function myexp(z::Complex)
-    er = exp(real(z))
-    s, c = sincos(imag(z))
-    return complex(er * c, er * s)
+import ForwardDiff
+import ForwardDiff: Dual
+
+# Fix for https://github.com/JuliaDiff/ForwardDiff.jl/issues/514
+function Base.exp(x::Complex{Dual{T,V,N}}) where {T,V,N}
+    xx = complex(ForwardDiff.value(real(x)), ForwardDiff.value(imag(x)))
+    dx = complex.(ForwardDiff.partials(real(x)), ForwardDiff.partials(imag(x)))
+
+    expv = exp(xx)
+    dexpv = expv * dx
+    complex(Dual{T,V,N}(real(expv), ForwardDiff.Partials{N,V}(tuple(real(dexpv)...))),
+            Dual{T,V,N}(imag(expv), ForwardDiff.Partials{N,V}(tuple(imag(dexpv)...))))
 end
 
+# Fix for https://github.com/JuliaDiff/ForwardDiff.jl/issues/514
+function Base.sqrt(x::Complex{Dual{T,V,N}}) where {T,V,N}
+    xx = complex(ForwardDiff.value(real(x)), ForwardDiff.value(imag(x)))
+    dx = complex.(ForwardDiff.partials(real(x)), ForwardDiff.partials(imag(x)))
 
-# Imaginary exponential compatible with autodiff
-@inline function mycis(x::Real)
-    s, c = sincos(x)
-    return complex(c, s)
-end
-@inline function mycis(z::Complex)
-    er = exp(-imag(z))
-    s, c = sincos(real(z))
-    return complex(er * c, er * s)
-end
-
-
-# Complex square root compatible with autodiff
-@inline function mysqrt(x::Real)
-    return sqrt(x)
-end
-@inline function mysqrt(z::Complex)
-    x = real(z)
-    y = imag(z)
-    r = hypot(x, y)
-    s = sqrt((r + x) / 2)
-    return complex(s, y / (2*s))
+    sqrtv = sqrt(xx)
+    dsqrtv = dx / (2 * sqrtv)
+    complex(Dual{T,V,N}(real(sqrtv), ForwardDiff.Partials{N,V}(tuple(real(dsqrtv)...))),
+            Dual{T,V,N}(imag(sqrtv), ForwardDiff.Partials{N,V}(tuple(imag(dsqrtv)...))))
 end

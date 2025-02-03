358096a820dcd3ef9ed7400df91df5e5c6743cff
Clement Guillot <clement.guillot@enpc.fr>
2025-02-03 10:30:55 +0100

    Changed eltype to core_type in array functions

--- src/hermite_array.jl
+++ src/hermite_array.jl
@@ -1,4 +1,7 @@
 #
+function core_type(::Type{AbstractVector{TG}}) where{TG<:AbstractWavePacket}
+    return core_type(TG)
+end
 function core_type(G::AbstractVector{TG}) where{TG<:AbstractWavePacket}
     return core_type(TG)
 end
@@ -6,8 +9,8 @@ end
 #=
     Computes ∑ₖ G[k](x)
 =#
-function (G::AbstractVector{TG})(x::T) where{TG<:AbstractWavePacket, T<:Number}
-    s = zero(promote_type(eltype(TG), T))
+function (G::AbstractVector{<:AbstractWavePacket})(x::T) where{T<:Number}
+    s = zero(promote_type(core_type(G), T))
     for g in G
         s += g(x)
     end
@@ -18,8 +21,8 @@ end
     Computes the integral
         ∫ ∑ₖG[k]
 =#
-function integral(G::AbstractVector{TG}) where{TG<:AbstractWavePacket}
-    s = zero(eltype(TG))
+function integral(G::AbstractVector{<:AbstractWavePacket})
+    s = zero(core_type(G))
     for g in G
         s += integral(g)
     end
@@ -30,8 +33,8 @@ end
     Computes the dot product
         ∑ₖ,ₗ dot_L2(G1[k], G2[l])
 =#
-function dot_L2(G1::AbstractVector{TG1}, G2::AbstractVector{TG2}) where{TG1<:AbstractWavePacket, TG2<:AbstractWavePacket}
-    s = zero(promote_type(eltype(TG1), eltype(TG2)))
+function dot_L2(G1::AbstractVector{<:AbstractWavePacket}, G2::AbstractVector{<:AbstractWavePacket})
+    s = zero(promote_type(core_type(G1), core_type(G2)))
     for g1 in G1
         for g2 in G2
             s += dot_L2(g1, g2)
@@ -44,8 +47,8 @@ end
     Computes the dot product
         ∑ₖ dot_L2(G1[k], G2)
 =#
-function dot_L2(G1::AbstractVector{TG1}, G2::TG2) where{TG1<:AbstractWavePacket, TG2<:AbstractWavePacket}
-    s = zero(promote_type(eltype(TG1), eltype(TG2)))
+function dot_L2(G1::AbstractVector{<:AbstractWavePacket}, G2::AbstractWavePacket)
+    s = zero(promote_type(core_type(G1), core_type(G2)))
     for g1 in G1
         s += dot_L2(g1, G2)
     end
@@ -56,8 +59,8 @@ end
     Computes the dot product
         ∑ₗ dot_L2(G1, G2[l])
 =#
-function dot_L2(G1::TG1, G2::AbstractVector{TG2})  where{TG1<:AbstractWavePacket, TG2<:AbstractWavePacket}
-    s = zero(promote_type(eltype(TG1), eltype(TG2)))
+function dot_L2(G1::AbstractWavePacket, G2::AbstractVector{<:AbstractWavePacket})
+    s = zero(promote_type(core_type(G1), core_type(G2)))
     for g2 in G2
         s += dot_L2(G1, g2)
     end
@@ -67,8 +70,8 @@ end
 #=
     Computes the squared L2 norm of ∑ₖ G[k]
 =#
-function norm2_L2(G::AbstractVector{TG}) where{TG<:AbstractWavePacket}
-    s = real(zero(eltype(TG)))
+function norm2_L2(G::AbstractVector{<:AbstractWavePacket})
+    s = real(zero(core_type(G)))
     for k in eachindex(G)
         s += norm2_L2(G[k])
         for l in k+1:length(G)

66d6a02aca9aa15768e5c6487c6d40cb159f4240
Clement Guillot <clement.guillot@enpc.fr>
2025-02-20 17:19:08 +0100

    Renamed test files

--- test/test.jl
+++ test/test.jl
@@ -28,8 +28,8 @@ function legendre_quadrature(X, M, F)
     return ComplexF64(I)
 end
 
-include("test_gaussian.jl");
-include("test_hermite.jl");
+include("test_gaussian1d.jl");
+include("test_hermite1d.jl");
 
-include("test_gaussian_wave_packet.jl");
-include("test_hermite_wave_packet.jl");
+include("test_gaussian_wave_packet1d.jl");
+include("test_hermite_wave_packet1d.jl");

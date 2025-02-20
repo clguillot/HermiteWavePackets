4e4c93fc9166904e1cce311c3d1ea387e555f8de
Clement Guillot <clement.guillot@enpc.fr>
2025-02-20 13:47:59 +0100

    Generic versions for some functions

---
+++ src/generics.jl
@@ -0,0 +1,16 @@
+
+function zero(::T) where{T<:AbstractWavePacket}
+    return zero(T)
+end
+
+function core_type(::T) where{T<:AbstractWavePacket}
+    return core_type(T)
+end
+
+function fitting_float(::T) where{T<:AbstractWavePacket}
+    return fitting_float(T)
+end
+
+@inline function norm_L2(G::AbstractWavePacket)
+    return sqrt(norm2_L2(G))
+end

fa8b23617a9e40922e2a9a0d1ce482ffd56e7521
Clement Guillot <clement.guillot@enpc.fr>
2025-03-22 22:51:46 +0100

    Created tensor contraction function for SArray's

--- src/utils.jl
+++ src/utils.jl
@@ -85,3 +85,94 @@ function clenshaw_hermite_eval(Λ::AbstractVector{TΛ}, z::Tz, x::SVector{M, Tx}
     end
     return u .* α0
 end
+
+#=
+
+    STATIC TENSOR CONTRACTION
+
+=#
+@generated function static_tensor_contraction(A::SArray{N, <:Number}, X::SVector...) where N
+    S = N.parameters
+    for (k, s, x) in zip(eachindex(S), S, X)
+        if length(x) != s
+            throw(DimensionMismatch("DimensionMismatch: Incompatible contraction length for $k-th index"))
+        end
+        if !(eltype(x)<:Number)
+            throw(ArgumentError("ArgumentError: Expecting element type of all vectors to be a subtype of $Number, but argument type for the $k-th contraction vector has element type $(eltype(x))"))
+        end
+    end
+    T = promote_type(eltype(A), (eltype.(X))...)
+    if length(X) == 0
+        return A
+    elseif length(X) > length(A)
+        throw(ArgumentError("ArgumentError: The number of vectors for contraction exceeds the available indices in A"))
+    elseif length(S) == length(X)
+        init_code = :( λ = zero($T) )
+        if eltype(first(X)) <: Complex
+            prep_code = quote
+                            re_X1 = real.(first(X))
+                            im_X1 = imag.(first(X))
+                        end
+        else
+            prep_code = :()
+        end
+        return_code = :( return λ )
+        loop_code = :()
+        array_access = Meta.parse("A[" * prod("j$k," for k in eachindex(S)) * "]")
+        array_access_drop = Meta.parse("A[" * ":," * prod("j$k," for k in Iterators.drop(eachindex(S), firstindex(S))) * "]")
+        for (ax, k, l) in zip(axes(A), 1:length(S), eachindex(X))
+            jk_symb = Symbol("j$k")
+            xk_symb = Symbol("x$k")
+            μk_symb = Symbol("μ$k")
+            μkp1_symb = Symbol("μ$(k+1)")
+            if k == 1
+                if eltype(first(X)) <: Complex
+                    if k == length(S)
+                        loop_code =
+                            :(
+                                λ += dot(re_X1, $array_access_drop) + im * dot(im_X1, $array_access_drop)
+                            )
+                    else
+                        loop_code =
+                            :(
+                                λ += $μkp1_symb * (dot(re_X1, $array_access_drop) + im * dot(im_X1, $array_access_drop))
+                            )
+                    end
+                else
+                    if k == length(S)
+                        loop_code =
+                            :(
+                                λ += dot(X[$l], $array_access_drop)
+                            )
+                    else
+                        loop_code =
+                            :(
+                                λ += $μkp1_symb * dot(X[$l], $array_access_drop)
+                            )
+                    end
+                end
+            else
+                if k == length(S)
+                    μ_code = :( $μk_symb = $xk_symb )
+                else
+                    μ_code = :( $μk_symb = $μkp1_symb * $xk_symb )
+                end
+                loop_code =
+                    :(
+                        for ($jk_symb, $xk_symb) in zip($ax, X[$l])
+                            $μ_code
+                            $loop_code
+                        end
+                    )
+            end
+        end
+        return quote
+            $prep_code
+            $init_code
+            $loop_code
+            $return_code
+        end
+    else
+        throw(ErrorException("Not implemented..."))
+    end
+end

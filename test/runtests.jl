75bb933a6612721434c68640e0568fc99548a935
Clement Guillot <clement.guillot@enpc.fr>
2025-04-01 14:54:41 +0200

    Removed Gaussian1D and GaussianWavePacket1D

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,9 @@
 include("test.jl")
 
-test_gaussian1d()
-println()
-
 test_hermite1d()
 println()
 
-test_gaussian_wave_packet1d()
-println()
-
 test_hermite_wave_packet1d()
-println()
-
-test_gaussian()
 println()
 
 test_gaussian_wave_packet()

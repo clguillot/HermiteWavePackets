904986554a3346453b4bc0262df79eeb03503b12
Clement Guillot <clement.guillot@enpc.fr>
2025-07-08 13:17:27 +0200

    Default promotion patterns

--- src/generics.jl
+++ src/generics.jl
@@ -48,6 +48,7 @@ function Base.iterate(::AbstractWavePacket, ::Nothing)
     return nothing
 end
 
-@inline function norm_L2(G::AbstractWavePacket)
-    return sqrt(norm2_L2(G))
-end
+Base.:*(G1::AbstractWavePacket{D}, G2::AbstractWavePacket{D}) where D = *(promote(G1, G2)...)
+convolution(G1::AbstractWavePacket{D}, G2::AbstractWavePacket{D}) where D = convolution(promote(G1, G2)...)
+dot_L2(G1::AbstractWavePacket{D}, G2::AbstractWavePacket{D}) where D = dot_L2(promote(G1, G2)...)
+norm_L2(G::AbstractWavePacket) = sqrt(norm2_L2(G))
